89ad4c8e36bb1203624538d2bc7cd700aac82c2f
anshu kumar <anshu.kumar@ebizontek.com>
2019-01-31 11:43:25 +0530

    dockerize

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ const app = express();
 const db = require('./app/db/db');
 const routes = require('./app/routes/index');
 const router = express.Router();
+const expressSwagger = require('express-swagger-generator')(app);
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'Node JS Starter Code',
+            title: 'Node Js Rest API',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/api/v1',
+        produces: [
+            "application/json"
+        ],
+        schemes: ['http', 'https'],
+		securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['*.js'] //Path to the API handle folder
+};
+
+expressSwagger(options)
+
+
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 
 app.use('/api/v1', routes(router));
 
