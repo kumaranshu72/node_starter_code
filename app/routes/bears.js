c8c9664a6504a56d69c475f1fea8e631dc209766
anshu kumar <anshu.kumar@ebizontek.com>
2019-01-31 11:14:09 +0530

    indentation

--- app/routes/bears.js
+++ app/routes/bears.js
@@ -2,15 +2,12 @@ const BearController = require('../Controller/BearController');
 
 
 module.exports = (router) => {
-
   router.get('/', function(req, res) {
       res.status(200).json({ message: 'hooray! welcome to our api!' });
   });
-
   router.route('/bears')
     .post(BearController.add)
-     .get(BearController.getAll);
-
+    .get(BearController.getAll);
   router.route('/bears/:bear_id')
     .get(BearController.getById)
     .put(BearController.update)
